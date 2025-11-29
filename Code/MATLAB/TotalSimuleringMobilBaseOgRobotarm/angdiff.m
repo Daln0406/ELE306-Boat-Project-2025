7f8934ef00ce79eabb29a3b758dbeee6df19b9aa
LarsHelge <668798@stud.hvl.no>
2025-11-29 15:01:37 +0100

    Update angdiff.m

--- Code/MATLAB/TotalSimuleringMobilBaseOgRobotarm/angdiff.m
+++ Code/MATLAB/TotalSimuleringMobilBaseOgRobotarm/angdiff.m
@@ -2,8 +2,7 @@ function d = angdiff(a, b)
 % Kompatibel angdiff:
 %  angdiff(x)        -> wrap x til (-pi, pi]
 %  angdiff(a, b)     -> vinkel(b - a) wrappet til (-pi, pi]
-%
-% Vektoriserbar og støtter skalar↔vektor via implisitt utvidelse.
+
 
 n = nargin;
 if n == 1
@@ -15,3 +14,4 @@ else
     error('angdiff: wrong number of inputs');
 end
 end
+

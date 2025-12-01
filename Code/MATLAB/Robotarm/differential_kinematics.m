95e0e4ad44d0929d230e8e1fa6fcf30310ff0993
bendikbp <48202511+bendikbp@users.noreply.github.com>
2025-12-01 10:41:55 +0100

    Add comments for DH parameter offsets in robot arm
    Added comments to clarify the purpose of offsets in the DH parameters.

--- Code/MATLAB/Robotarm/differential_kinematics.m
+++ Code/MATLAB/Robotarm/differential_kinematics.m
@@ -12,8 +12,8 @@ L2 = Link([0 d2 a2 alpha_2 0]);
 L3 = Link([0 d3 a3 alpha_3 0]);
 L4 = Link([0 d4 a4 alpha_4 0]);
 L5 = Link([0 d5 a5 alpha_5 0]);
-L2.offset = -pi/2;
-L4.offset = pi/2;
+L2.offset = -pi/2; % Offset på leddvinkel fra DH-tabellen
+L4.offset = pi/2; % Offset på leddvinkel fra DH-tabellen
 
 % Sett sammen leddene til et SerialLink-objekt
 robot = SerialLink([L1 L2 L3 L4 L5],'name','5DOF');
